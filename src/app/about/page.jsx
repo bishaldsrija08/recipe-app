b568186c894d6e790c494bdd8fdb749fd268ab5b
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-07-06 20:03:58 +0545

    UI part done

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+
+const About = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold text-center mt-10">About Us</h1>
+        <p className="text-center mt-4 max-w-2xl mx-auto">
+            Welcome to our recipe app! We are passionate about food and love sharing delicious recipes with everyone. Our goal is to inspire home cooks of all levels to create amazing dishes using simple ingredients. Whether you're looking for quick weeknight meals or elaborate weekend feasts, we have something for you. Join us on this culinary journey and let's cook together!
+        </p>
+    </div>
+  )
+}
+
+export default About
