e081aac97f9c0cde62e0e58bc9a87ae87fc4db19
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-07-08 22:42:25 +0545

    get and post recipe

--- src/app/api/recipe/route.js
+++ src/app/api/recipe/route.js
@@ -1,5 +1,26 @@
+import { db } from "@/db/db"
+import { recipes } from "@/db/schema"
+
 export async function GET() {
+    const  data = await db.select().from(recipes)
     return Response.json({
-        message: "Hello from the recipe API!"
-    })
+        message: "Data fetched successfully!",
+        data
+    }, { status: 200 })
+}
+
+//insert recipe to recipe table
+export async function POST(request) {
+    const data = await request.json()
+    try {
+        await db.insert(recipes).values(data)
+        return Response.json({
+            message: "Recipe created successfully!"
+        })
+    } catch (error) {
+        return Response.json({
+            message: "Error creating recipe",
+            error: error.message
+        }, { status: 500 })
+    }
 }
---
+++ src/app/api/recipe/route.ts
@@ -0,0 +1,7 @@
+
+
+export async function GET(request, ) {
+    return Response.json({
+        message: "Hello from the recipe API!"
+    }, { status: 200 });
+}
